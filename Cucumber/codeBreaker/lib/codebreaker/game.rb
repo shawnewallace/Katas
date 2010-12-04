4ab4cf5330ae72feb16bf20601c93598062e1594
Shawn Wallace <shawnewallace@gmail.com>
2010-12-04 10:22:01 -0500

    rspec next chapter

--- Cucumber/codeBreaker/lib/codebreaker/game.rb
+++ Cucumber/codeBreaker/lib/codebreaker/game.rb
@@ -1,12 +1,37 @@
 module Codebreaker
-	class Game
-		def initialize(output)
-			@output = output
-		end
+  class Game
+    def initialize(output)
+      @output = output
+    end
 
-		def start
-			@output.puts 'Welcome to Codebreaker!'
-			@output.puts 'Enter guess:'
-		end
-	end
+    def start(secret)
+      @secret = secret
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
+    end
+
+    def guess(guess)
+      @output.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
+    end
+    
+    def exact_match_count(guess)
+      (0..3).inject(0) do |count, index|
+        count + (exact_match?(guess, index) ? 1 : 0)
+      end
+    end
+    
+    def number_match_count(guess)
+      (0..3).inject(0) do |count, index|
+        count + (number_match?(guess, index) ? 1 : 0)
+      end
+    end
+    
+    def exact_match?(guess, index)
+      guess[index] == @secret[index]
+    end
+    
+    def number_match?(guess, index)
+      @secret.include?(guess[index])
+    end
+  end
 end
