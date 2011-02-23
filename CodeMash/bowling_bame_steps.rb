1ae85619a6dc800f80af62f55698a9cc157efa64
Shawn Wallace <shawnewallace@gmail.com>
2011-02-23 11:26:05 -0500

    added feature file and step definitions

---
+++ CodeMash/bowling_bame_steps.rb
@@ -0,0 +1,25 @@
+require 'bowling_game'
+
+
+Before do
+  @game = Game.new
+  @score = 0
+end
+
+Given /^I make no rolls$/ do
+end
+
+When /^I score it$/ do
+  @score = @game.Score
+end
+
+Then /^My score should be (\d+)$/ do |expected|
+  
+
+  @score.should eq(Integer(expected))
+end
+
+Given /^I make the following rolls:$/ do |table|
+  table.hashes.each {|roll| @game.Roll(roll[:roll])}
+end
+
