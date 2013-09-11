a46238e61e2286cd6e5f47e491adadef5b885532
Shawn Wallace <shawnewallace@gmail.com>
2013-09-11 16:39:42 -0400

    Finised basic game

--- sudoku/cs/SudokuSolver/Sudoku.con/Program.cs
+++ sudoku/cs/SudokuSolver/Sudoku.con/Program.cs
@@ -13,8 +13,9 @@ namespace Sudoku.con
 	{
 		static void Main(string[] args)
 		{
+			//var game = new SodukuGame("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
 			var game = new SodukuGame("4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......");
-
+			
 			Console.Write(game.ToString());
 			
 			Console.ReadLine();
